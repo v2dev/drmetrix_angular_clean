9b03b11d3cc67d6052ea07f456beb80e9cf7a5e7
surajverma833 <52952436+surajverma833@users.noreply.github.com>
2020-02-28 19:32:06 +0530

    User section with new angular ui grid -1

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-var drmApp = angular.module('drmApp', ['ui.router','starter.services','ngCookies','ui.bootstrap']);
+var drmApp = angular.module('drmApp', ['ui.router', 'starter.services', 'ngCookies', 'ui.bootstrap', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.autoResize', 'ui.grid.expandable',
+    'ui.grid.selection', 'ui.grid.treeView', 'ui.grid.exporter']);
 
-drmApp.config(function($stateProvider, $urlRouterProvider) {
-    
+drmApp.config(function ($stateProvider, $urlRouterProvider) {
+
     $urlRouterProvider.otherwise('/home');
-    
+
     $stateProvider
-        
+
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
             templateUrl: 'templates/home.html',
-			controller: 'LoginController'
-		})
+            controller: 'LoginController'
+        })
         .state('authy', {
-			url: '/authy',
+            url: '/authy',
             templateUrl: 'templates/authy.html',
             controller: 'AuthyController'
         })
         .state('eulaAgreement', {
-			url: '/eulaAgreement',
+            url: '/eulaAgreement',
             templateUrl: 'templates/eulaAgreement.html',
             controller: 'EulaAgreementController'
         })
         .state('ranking', {
-			url: '/ranking',
+            url: '/ranking',
             templateUrl: 'templates/ranking.html',
             controller: 'RankingController'
         })
-        
+        .state('userAccount', {
+            url: '/userAccount',
+            templateUrl: 'templates/userAccount.html',
+            controller: 'UserController'
+        })
+
         // // nested list with custom controller
         // .state('home.list', {
         //     url: '/list',
@@ -36,12 +42,12 @@ drmApp.config(function($stateProvider, $urlRouterProvider) {
         //         $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
         //     }
         // })
-        
+
         // nested list with just some random string data
         .state('forgotPassword', {
-			url: '/forgotPassword',
+            url: '/forgotPassword',
             templateUrl: 'templates/forgot-password.html',
-			controller: 'LoginController'
+            controller: 'LoginController'
         });
 });
 
---
+++ assets/js/controllers/user.js
@@ -0,0 +1,34 @@
+angular.module('drmApp').controller('UserController', function ($scope, $http, $interval, uiGridTreeViewConstants, $rootScope, apiService) {
+    var usCtrl = this;
+    $scope.showUsers = function () {
+        var config = {
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        }
+        $scope.gridOptions1 = {};
+        $scope.gridOptions1 = {
+            columnDefs: [
+                { name: 'name' },
+                { name: 'gender', enableHiding: false },
+                { name: 'company' }
+            ],
+
+            onRegisterApi: function (gridApi) {
+                $scope.gridApi = gridApi;
+            }
+        };
+
+        // apiService.post('./../../assets/json/100.json', config)
+        apiService.post('/show_users', config)
+            .then(function (response) {
+                console.log(response.status);
+                console.log(response.result);
+                console.log(response.count);
+                $scope.gridOptions1.data = response.status;
+            });
+
+    }
+    $scope.showUsers();
+
+});
