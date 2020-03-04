95cd8d374a884f7a74aac0e67bf638b7d1edc0c2
surajverma833 <52952436+surajverma833@users.noreply.github.com>
2020-03-04 15:31:07 +0530

    Ranking primary Filter edit,cancel -1

--- assets/js/controllers/ranking.js
+++ assets/js/controllers/ranking.js
@@ -4,4 +4,27 @@ angular.module("drmApp").controller("RankingController", function($scope, $state
         $state.go('home');
         return;
     }
+
+    $scope.rankingPrimaryFiler = 0;
+
+    $scope.editable = function () {
+        $scope.rankingPrimaryFiler = 1;
+
+    }
+
+    $scope.applicable = function () {
+        console.log();
+        $scope.rankingPrimaryFiler = 0;
+
+    }
+
+    $scope.resettable = function () {
+        $scope.rankingPrimaryFiler = 0;
+    }
+
+    $scope.cancelable = function () {
+        $scope.rankingPrimaryFiler = 0;
+
+    }
+
 });
