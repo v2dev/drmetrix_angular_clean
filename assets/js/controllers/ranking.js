926b6e69b2329fe6bb05de663144fd2681e2dc26
surajverma833 <52952436+surajverma833@users.noreply.github.com>
2020-03-09 18:02:06 +0530

    Ranking page Advertiser angular UI Grid and subgrid-5

--- assets/js/controllers/ranking.js
+++ assets/js/controllers/ranking.js
@@ -236,7 +236,9 @@ angular.module("drmApp").controller("RankingController", function($scope, $http,
                             columnDefs: [
                             // { name: 'id', displayName:'id', width:'50' },
                             { name: 'brand_name', displayName:'Brand Name', cellTemplate:'<i class="fa fa-circle" id="{{data.rows.is_brand_active == 1 ? \'active_btn\' : \'inactive_btn\'}}"></i><span><a href="#" title="{{row.entity.advertiser_name}}" ng-click="view_adv_tab({{row.entity.advertiser_name}},{{row.entity.id}},'+c_dir+','+formData.type+','+formData.val+','+formData.sd+','+formData.ed+',\'brand\',{{row.entity.id}},{{COL_FIELD}},\'ranking\',{{row.entity.need_help}})" >{{COL_FIELD}}</a></span>' },
-                            { name: 'creatives_count', displayName: 'Creatives', cellTemplate:'<a href=""><i class="clickable ng-scope ui-grid-icon-plus-squared" ng-if="!(row.groupHeader==true || row.entity.subGridOptions.disableRowExpandable)" ng-class="{\'ui-grid-icon-plus-squared\' : !row.isExpanded, \'ui-grid-icon-minus-squared\' : row.isExpanded }" ng-click="grid.api.expandable.toggleRowExpansion(row.entity, $event)"></i>{{COL_FIELD}}</a>' },
+
+                            { name: 'creatives_count', displayName: 'Creatives', cellTemplate:'<a href=""><i class="clickable ng-scope ui-grid-icon-plus-squared" ng-if="!(row.groupHeader==true || row.entity.subGridOptions2.disableRowExpandable)" ng-class="{\'ui-grid-icon-plus-squared\' : !row.isExpanded, \'ui-grid-icon-minus-squared\' : row.isExpanded }" ng-click="grid.api.expandable.toggleRowExpansion(row.entity, $event)"></i>{{COL_FIELD}}</a>' },
+
                             { name: 'category_name', displayName:'Category', cellTemplate:'<a href="#"><span ng-if="row.entity.category_name!=\'\'" class="tooltip-hover" ng-click="fetchList({{row.entity.id}},'+formData.type+',{{COL_FIELD}});"><i class="fa fa-caret-down float-right"></i>{{COL_FIELD}} <div class="cat_col_dropdown select_cat_dropdown" id="cat_col_dropdown_row.entity.id" style="display:none;"></div></span><span ng-if="row.entity.category_name==\'\'"> - </span></a>' },
                             { name: 'airings', displayName:'Airings', cellTemplate:'<a href="#"><span ng-if="row.entity.airings!=\'\'" class="ranking_airings" ng-click="viewAiringGraph({{row.entity.brand_name}},{{row.entity.id}},\'dow\','+formData.network_code+',\'all_day\',\'all_hour\',{{row.entity.networks}},{{row.entity.airings}},'+formData.c+','+formData.type+','+formData.val+','+formData.sd+','+formData.ed+','+formData.responseType+','+formData.spanish+',\'brand\',\'\',\'\',\'\');">{{COL_FIELD}} </span><span ng-if="row.entity.airings==\'\'"> - </span></a>'  },
                             { name: 'spend_index', displayName: 'Spend',  cellTemplate: '<a href="#"><span ng-if="row.entity.spend_index!=\'\'" class="ranking_airings" ng-click="viewAiringSpendGraph({{row.entity.brand_name}},{{row.entity.id}},\'dow\','+formData.network_code+',\'all_day\',\'all_hour\',{{row.entity.networks}},{{row.entity.spend_index}},'+formData.c+','+formData.type+','+formData.val+','+formData.sd+','+formData.ed+','+formData.responseType+','+formData.spanish+',\'brand\',\'\',\'\',\'\');">{{COL_FIELD}}</span><span ng-if="row.entity.spend_index==\'\'"> - </span></a>' },
@@ -245,15 +247,43 @@ angular.module("drmApp").controller("RankingController", function($scope, $http,
                             { name: 'asd', displayName:'ADS'},
                             { name: 'total_weeks', displayName: 'Weeks' },
                             { name: 'tracking', displayName: "Tracking", cellTemplate: '<a href="#"><i custom-attr="brand_{{row.entity.id}}" class="fa fa-eye-slash grey-eye" title="Track"></i></a>' }
-                        ],enableGridMenu: true,
-                          enableSelectAll: true,
-                          paginationPageSize: 10,
-                          paginationTemplate: correctTotalPaginationTemplate,
+                        ],expandableRowTemplate: '/drmetrix/templates/expandableRowTemplate.html',
+                            expandableRowHeight: 200,
+                            enableGridMenu: true,
+                            enableSelectAll: true,
+                            paginationPageSize: 5,
+                            enableSorting: true,
+                            showTreeExpandNoChildren: false,
+                            enableExpandableRowHeader: false,
+                            enableRowSelection: true,
+                            paginationTemplate: correctTotalPaginationTemplate,
+
+                            onRegisterApi: function (gridApi2) {
+                                gridApi2.expandable.on.rowExpandedStateChanged($scope, function (Secondrow) {
+                                    formData.network_code = '';
+                                    formData.programs_ids = '';
+                                    formData.is_adv_page  = 0;
+                                    formData.brand_id = Secondrow.entity.id;
+                                    formData.tab = 'adv_brand';
+                                    if (Secondrow.isExpanded) {
+                                        Secondrow.entity.subGridOptions2 = {
+                                            columnDefs: [
+                                                { name: 'id', displayName:'id' },
+                                            ]
+                                        };
+                                        apiService.post('/brand_creatives', formData, config)
+                                        .then(function (data) {
+                                            Secondrow.entity.subGridOptions2.data = data.data.rows;
+                                        }, function (response) {
+                                            // this function handlers error
+                                        });
+                                    }
+                                });
+                            },
                         };
 
                         apiService.post('/adv_brand', formData, config)
                         .then(function (data) {
-                            console.log(data.data);
                             row.entity.subGridOptions.data = data.data.rows;
                         }, function (response) {
                             // this function handlers error
@@ -289,7 +319,6 @@ angular.module("drmApp").controller("RankingController", function($scope, $http,
     $scope.initializeRankingPage = function() {
     $scope.formdata =  {"sd":"2020-02-24","ed":"2020-03-01","startDate":1,"val":1,"c":1,"type":1,"cat":"all","flag":2,"spanish":"0,1","responseType":"(response_url = 1 or response_mar = 1 or response_sms = 1 or response_tfn = 1 )","unchecked_category":"","length_unchecked":0,"creative_duration":"10,15,20,30,45,60,75,90,105,120,180,240,300","new_filter_opt":"none","lifetime_flag":false,"all_ytd_flag":false,"refine_filter_opt":"","refine_filter_opt_text":"","refine_apply_filter":0,"applied_ids":"","primary_tab":""}
 
-        console.log($scope.type);
         ($scope.type != 'brands') ? $scope.uigridDataBrand() : $scope.uigridDataAdv();
     }
     // $scope.uigridDataBrand(formdata);
